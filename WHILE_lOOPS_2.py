c00b8dd3a5b86eaabeacb6ab9b9d28100aacfbf5
Srinu Duda <128722536+Srinududa@users.noreply.github.com>
2023-04-05 22:18:47 +0530

    Click here to view the coding

---
+++ WHILE_lOOPS_2.py
@@ -0,0 +1,24 @@
+a = int(input())
+b = int(input())
+counter = 1
+while counter<=a:
+    s=(str(counter) + " ") *b
+    print(s)
+    counter=counter+1
+result==>>
+1 1 1 1 
+2 2 2 2 
+3 3 3 3 
+4 4 4 4 
+5 5 5 5 
+
+
+a = 2
+count=0
+while count<a:
+    print(count)
+    count=count+1
+
+result==>>
+0
+1
